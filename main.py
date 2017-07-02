dd2e34c7ac12889f6e6920e28e93d6f0c6d65bf4
Michael Alarcon <malarconii@yahoo.com>
2017-07-02 14:13:53 -0700

    Finished blog page listing, added functionality to create a new blog post with database functionality

--- main.py
+++ main.py
@@ -4,25 +4,47 @@ import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/get-it/done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'dillybar'
+
+class Blog(db.Model):
+    '''User text input is taken in to create new blog posts'''
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(12000))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
 
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+
+    if request.method == 'POST':
+        blog_title = request.form['title']
+        blog_body = request.form['body']
+        new_post = Blog(blog_title, blog_body)
+        db.session.add(new_post)
+        db.session.commit()
+
+    return render_template('index.html', title='Blog Home Page')
 
 
-@app.route('/add-post')
+@app.route('/newpost')
 def add_post():
-    return render_template('add-post.html')
+    return render_template('newpost.html')
 
-@app.route('/blog-listings')
+@app.route('/blog')
 def blog_listings():
-    return render_template('blog-listings.html')
+
+    posts = Blog.query.all()
+
+    return render_template('blog.html', posts=posts)
 
 
 if __name__ == '__main__':
