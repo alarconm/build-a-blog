db2d6ff069bd7a6f2ec4cabf133cb2c2a194adab
Michael Alarcon <malarconii@yahoo.com>
2017-07-05 20:18:34 -0700

    Updated styling, added more comments

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ db = SQLAlchemy(app)
 app.secret_key = 'dillybar'
 
 class Blog(db.Model):
-    '''User text input is taken in to create new blog posts'''
+    '''Blog model that creates each blog post takes in title, body, pub_date'''
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
@@ -19,6 +19,7 @@ class Blog(db.Model):
     pub_date = db.Column(db.DateTime)
 
     def __init__(self, title, body, pub_date=None):
+        '''Initial parameters for the Blog class'''
         self.title = title
         self.body = body
         if pub_date is None:
@@ -26,14 +27,15 @@ class Blog(db.Model):
         self.pub_date = pub_date
 
 
-
 @app.route('/', methods=['POST', 'GET'])
 def index():
-
+    '''Displays the home page. Receives post from newpost form and redirects to blog page if requirements are met'''
+    
     if request.method == 'POST':
         blog_title = request.form['blog_title']
         blog_body = request.form['body']
 
+        # validate that a user entered a title or body, flash errors if not valid
         if not blog_body or not blog_title:
             if not blog_title:
                 flash('Please enter a title', 'error')
@@ -41,22 +43,23 @@ def index():
                 flash('Please enter content for your post', 'error')
             return render_template('newpost.html', title="New Blog Post")
 
-
         new_post = Blog(blog_title, blog_body)
         db.session.add(new_post)
         db.session.commit()
         return render_template('blogpage.html', post=new_post)
-
 
     return render_template('index.html', title='Blog Home Page')
 
 
 @app.route('/newpost')
 def add_post():
+    '''Display the new post template'''
+
     return render_template('newpost.html')
 
 @app.route('/blog')
 def blog_listings():
+    '''Display all blogs in the database, or just a specific post if an ID is passed in the GET'''
 
     posts = Blog.query.order_by(Blog.pub_date.desc()).all()
 
